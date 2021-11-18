493b3fb4c92e5ccf4703758de61839c9cff9640e
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-18 14:14:55 -0500

    api url changed, it now calling days of data also calling my favorite stocks

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ function getColor(stock) {
     if (stock === "GME") {
         return 'rgba(61, 161, 61, 0.7)'
     }
-    if (stock === "MSFT") {
+    if (stock === "AMC") {
         return 'rgba(209, 4, 25, 0.7)'
     }
-    if (stock === "DIS") {
+    if (stock === "AAPL") {
         return 'rgba(18, 4, 209, 0.7)'
     }
-    if (stock === "BNTX") {
+    if (stock === "LCID") {
         return 'rgba(166, 43, 158, 0.7)'
     }
 }
@@ -18,11 +18,11 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart')
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart')
     const averagePriceChartCanvas = document.querySelector('#average-price-chart')
-    const api_url = "https://api.twelvedata.com/time_series?symbol=GME,AMC,AAPL,ALF&interval=1min&apikey=b191a421d6e6468ab03e1c1bedbb82f1"
+    const api_url = "https://api.twelvedata.com/time_series?symbol=GME,AMC,AAPL,LCID&interval=1day&apikey=b191a421d6e6468ab03e1c1bedbb82f1"
     const response = await fetch(api_url)
     const data = await response.json()
-    const { GME, AMC, AAPL, ALF } = data
-    const stocks = [GME, AMC, AAPL, ALF]
+    const { GME, AMC, AAPL, LCID } = data
+    const stocks = [GME, AMC, AAPL, LCID]
 
     // console.log(data)
     stocks.forEach(stock => stock.values)
@@ -41,9 +41,41 @@ async function main() {
         }
     });
 
-
-
-
+    // const ctx = document.getElementById('myChart');
+    // const myChart = new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //         datasets: [{
+    //             label: '# of Votes',
+    //             data: [12, 19, 3, 5, 2, 3],
+    //             backgroundColor: [
+    //                 'rgba(255, 99, 132, 0.2)',
+    //                 'rgba(54, 162, 235, 0.2)',
+    //                 'rgba(255, 206, 86, 0.2)',
+    //                 'rgba(75, 192, 192, 0.2)',
+    //                 'rgba(153, 102, 255, 0.2)',
+    //                 'rgba(255, 159, 64, 0.2)'
+    //             ],
+    //             borderColor: [
+    //                 'rgba(255, 99, 132, 1)',
+    //                 'rgba(54, 162, 235, 1)',
+    //                 'rgba(255, 206, 86, 1)',
+    //                 'rgba(75, 192, 192, 1)',
+    //                 'rgba(153, 102, 255, 1)',
+    //                 'rgba(255, 159, 64, 1)'
+    //             ],
+    //             borderWidth: 1
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             y: {
+    //                 beginAtZero: true
+    //             }
+    //         }
+    //     }
+    // });
 
 
 

788649c70b72fe61037e8b2b27c57883f0c98555
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-18 12:48:09 -0500

    making http request

--- public/index.js
+++ public/index.js
@@ -3,7 +3,11 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const api_url = "https://api.twelvedata.com/time_series?symbol=GME,BNTX,DIS,MSFT&interval=1min&apikey=b191a421d6e6468ab03e1c1bedbb82f1"
 
+    let response = await fetch(api_url)
+    let data = await response.json()
+    console.log(data)
 }
 
 main()
